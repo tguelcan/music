a751cfad92c91e4ba6298c5d67867ce17991120c
Tayfun Gülcan <tayfun.guelcan@rpc-partners.com>
2023-04-12 01:36:02 +0200

    style: hover effect

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -7,20 +7,30 @@
 	$: ({ NewAlbums } = data);
 </script>
 
-<h1 class="font-serif text-2xl">New Album Releases</h1>
+<div class="container my-4">
+	<h1>New Album Releases</h1>
 
-<div class="flex flex-wrap gap-4">
-	{#each NewAlbums.items as { name, images, artists }}
-		<div>
-			<div class="w-36 rounded-2xl overflow-hidden">
-				<img alt="Albums" srcset={generateSrcset(images)} />
-			</div>
-			<dl>
-				<dt class="w-36 font-bold truncate text-primary-dark">{name}</dt>
+	<div class="flex flex-wrap gap-4 mt-4">
+		{#each NewAlbums.items as { name, images, artists }}
+			<div>
+				<a href="/" class="group">
+					<div class="w-36 rounded-2xl overflow-hidden relative">
+						<span class="sr-only">{name}</span>
+						<div
+							class="group-hover:bg-primary mix-blend-multiply group-hover:mix-blend-multiply transition-all absolute inset-0"
+						/>
+						<img alt="Albums" srcset={generateSrcset(images)} />
+					</div>
+					<div
+						class="w-36 font-bold truncate text-primary-dark dark:text-primary-light group-hover:text-primary transition-all"
+					>
+						{name}
+					</div>
+				</a>
 				{#each artists as artist}
-					<dd class="text-sm">{artist.name}</dd>
+					<div class="text-sm"><a href="/">{artist.name}</a></div>
 				{/each}
-			</dl>
-		</div>
-	{/each}
+			</div>
+		{/each}
+	</div>
 </div>
