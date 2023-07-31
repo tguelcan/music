fbb58360c1a854098d48ae2367da65ba9e2360b8
JEFF OKOLI <jahbuchimokoli3@gmail.com>
2023-07-31 22:39:37 +0100

    created a custom error page for handling error displays better

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+
+<div class="flex items-center justify-center flex-col pt-12">
+    <h1>{$page.error?.message}</h1>
+<p>An Error Occurred</p>
+</div>
