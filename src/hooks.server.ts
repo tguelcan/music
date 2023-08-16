fa8577937365e1ca73e5c8a46a9bf820b9f6cbad
JEFF OKOLI <jahbuchimokoli3@gmail.com>
2023-08-16 11:55:19 +0100

    fix: Refresh user token on 401 errors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,33 +3,35 @@ import { dev } from '$app/environment';
 import { setLanguage, getToken } from '$server';
 let cookieName = 'app';
 
-export const handle = (async ({ event, resolve }) => {
-	const appToken = event.cookies.get(cookieName);
 
-	/**
-	 * Get app access token and set cookie
-	 * Doc: https://developer.spotify.com/documentation/web-api/tutorials/getting-started
-	 */
-	if (!appToken) {
-		// Get data from response
-		const { access_token, expires_in } = await getToken();
-
-		// Set cookie
-		event.cookies.set(cookieName, access_token, {
+const setCookie = async (eventValue:any) => {
+	const { access_token, expires_in } = await getToken();
+	// Set cookie
+		eventValue.cookies.set(cookieName, access_token, {
 			path: '/',
 			httpOnly: true,
 			sameSite: 'strict',
 			secure: !dev,
 			maxAge: expires_in * 1000
 		});
+}
+
+export const handle = (async ({ event, resolve }) => {
+	const appToken = event.cookies.get(cookieName);
+	/**
+	 * Get app access token and set cookie
+	 * Doc: https://developer.spotify.com/documentation/web-api/tutorials/getting-started
+	 */
+	if (!appToken) {
+		// Get data from response
+		await setCookie(event)
 	}
 
 	const response = await resolve(event);
-
 	return response;
 }) satisfies Handle;
 
-export const handleFetch = (({ event, request, fetch }) => {
+export const handleFetch =  (async ({ event, request, fetch }) => {
 	const appToken = event.cookies.get(cookieName);
 	const { pathname } = new URL(request.url);
 
@@ -38,6 +40,10 @@ export const handleFetch = (({ event, request, fetch }) => {
 		request = setLanguage(event, request);
 		request.headers.set('Authorization', `Bearer ${appToken}`);
 	}
-
-	return fetch(request);
+	const res = await fetch(request)
+	// refetch token when it expires
+	if(res.status === 401){
+		await setCookie(event)
+	}
+	return res;
 }) satisfies HandleFetch;
